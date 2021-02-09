58f4d6551b2f40ff5eb033d0a8f24602d37497c1
Paul Mayer <paul@paul-mayer.eu>
2021-02-09 21:21:51 +0100

    test memory rxdb

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,27 +1,29 @@
-
-import { addRxPlugin,createRxDatabase } from 'rxdb';
+import {
+  addRxPlugin,
+  createRxDatabase
+} from 'rxdb';
 addRxPlugin(require('pouchdb-adapter-memory'));
 
 
-const serverDB = createRxDatabase({
+/*const serverDB = createRxDatabase({
   name: 'nextrxtestserver',           // <- name
   adapter: 'memory',          // <- storage-adapter
  // password: 'myPassword',     // <- password (optional)
  // multiInstance: true,         // <- multiInstance (optional, default: true)
   //eventReduce: false // <- eventReduce (optional, default: true)
-}).then(console.dir);
+}).then(console.dir);*/
 
 export default function Home() {
   const db = createRxDatabase({
-    name: 'nextrxtest',           // <- name
-    adapter: 'memory',          // <- storage-adapter
-   // password: 'myPassword',     // <- password (optional)
-   // multiInstance: true,         // <- multiInstance (optional, default: true)
+    name: 'nextrxtest', // <- name
+    adapter: 'memory', // <- storage-adapter
+    // password: 'myPassword',     // <- password (optional)
+    // multiInstance: true,         // <- multiInstance (optional, default: true)
     //eventReduce: false // <- eventReduce (optional, default: true)
   }).then(console.dir);
   return (
     <div className="container">
-     container
+      container
     </div>
   )
 }
